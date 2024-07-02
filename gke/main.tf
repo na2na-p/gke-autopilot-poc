b089285dd5d71d5400b3127d251c652e34cda4ed
na2na-p <na2na@na2na.dev>
2024-07-02 10:39:33 +0900

    暗黙オプションの明示指定

--- gke/main.tf
+++ gke/main.tf
@@ -4,13 +4,25 @@ provider "google" {
 }
 
 resource "google_container_cluster" "autopilot_cluster" {
-  name     = var.cluster_name
-  location = var.region
+  name             = var.cluster_name
+  location         = var.region
+  enable_autopilot = true
+  allow_net_admin  = true
   cost_management_config {
     enabled = true
   }
   deletion_protection = false # HACK: 検証用
-  initial_node_count = 1 # autopilotはリージョンクラスターなので必須
+  initial_node_count  = 1     # autopilotはリージョンクラスターなので必須
+
+  addons_config {
+    gcs_fuse_csi_driver_config {
+      enabled = true
+    }
+  }
+
+  cluster_autoscaling {
+    enabled = true
+  }
 }
 
 output "kubeconfig" {
